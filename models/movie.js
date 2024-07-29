0832909e0bc599e3b48bf892c81ce73ba428e0fa
Ram-Jam5 <edgeillusive@gmail.com>
2024-07-29 10:30:16 -0500

    add commentSchema as it was left off previous commit.

--- models/movie.js
+++ models/movie.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
 
+const commentSchema = new mongoose.Schema(
+    {
+        text: {
+            type: String,
+            required: true
+          },
+          author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    },
+    { timestamps: true }
+);
+
 const reviewSchema = new mongoose.Schema(
     {
         title: {
@@ -18,6 +29,7 @@ const reviewSchema = new mongoose.Schema(
     },
     { timestamps:true }
 );
+
 
 const movieSchema = new mongoose.Schema(
     {
